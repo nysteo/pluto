4f770080a4a0c1d558d5b888b05e00b34df8a21b
nysteo <teonys@nyu.edu>
2020-09-08 19:26:15 -0400

    setting up electron

---
+++ app/public/main.js
@@ -0,0 +1,16 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 900, height: 680});
+    mainWindow.loadURL('http://localhost:3000');
+
+    app.setAboutPanelOptions({ aplicationName: "Pluto", applicationVersion: "0.0.1"});
+    mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {if (process.platform !== "darwin") {app.quit(); }});
+app.on('activate', () => {if (mainWindow === null) {createWindow()}});
