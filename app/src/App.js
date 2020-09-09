c5a8e5113e27c542d86a712028aed5d1daa2c2cc
nysteo <teonys@nyu.edu>
2020-09-08 20:02:29 -0400

    setting up split planel

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SplitPane from 'react-split-pane';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <SplitPane split = "vertical" defaultSize = "50%">
+        <div className = "editor-panel">
+        </div>
+        <div className = "view-pane">          
+        </div>
+      </SplitPane>
     </div>
   );
+  
 }
 
 export default App;
