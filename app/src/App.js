f4234bb3bd3bc609e3650679827bdf5e088d78f4
nysteo <teonys@nyu.edu>
2020-09-08 20:17:15 -0400

    changing browserlist

--- app/src/App.js
+++ app/src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 import SplitPane from 'react-split-pane';
 
 function App() {
-  return (
-    <div className = "App">
-      <SplitPane split = "vertical" defaultSize = "50%">
-        <div className = "editor-panel">
-        </div>
-        <div className = "view-pane">          
-        </div>
-      </SplitPane>
-    </div>
-  );
+    return (
+      <div className = "App">
+        <SplitPane split = "vertical" defaultSize = "50%">
+          <div className = "editor-panel">
+          </div>
+          <div className = "view-pane">          
+          </div>
+        </SplitPane>
+      </div>
+    );
   
 }
 
