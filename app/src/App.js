5140129d8fb8918b3dbfbc6df2159779021006ef
nysteo <teonys@nyu.edu>
2020-09-09 01:12:04 -0400

    i guess it works?

--- app/src/App.js
+++ app/src/App.js
@@ -3,19 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import SplitPane from 'react-split-pane';
 import Editor from './editor.js';
+import ReactMarkdown from 'react-markdown';
+import MarkdownRender from './markdownrender.js'
+
 class App extends Component {
   constructor(props) {
     super();
     this.state = { markdownSrc: "# Hello World" , }
+    this.onMarkdownChange = this.onMarkdownChange.bind(this);
+
   }
+  onMarkdownChange(md) { this.setState({markdownSrc: md});}
   render() {
     return (
       <div className = "App">
         <SplitPane split = "vertical" defaultSize = "50%">
           <div className = "editor-panel">
-            <Editor className = "editor" value = {this.state.markdownSrc}/>
+            <Editor className = "editor" value = {this.state.markdownSrc} onChange = {this.onMarkdownChange}/>
           </div>
-          <div className = "view-pane">          
+          <div className = "view-pane">
+            <MarkdownRender source = {this.state.markdownSrc} />
           </div>
         </SplitPane>
       </div>
--- app/src/editor.js
+++ app/src/editor.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import CodeMirror from '@skidding/react-codemirror';
+
 require('codemirror/lib/codemirror.css');
 require('codemirror/mode/javascript/javascript');
 require('codemirror/mode/python/python');
 require('codemirror/mode/xml/xml');
-require('codemirror/mode/stex/stex');
 require('codemirror/mode/markdown/markdown');
-require('codemirror/theme/monokai.css');
+require('codemirror/theme/erlang-dark.css');
 
 class Editor extends Component {
+    constructor(props) {
+        super(props);
+        this.updateCode = this.updateCode.bind(this);
+    }
+    updateCode(e) { this.props.onChange(e);}
     render() {
         var options = {
             mode: 'stex',
-            theme: 'monokai',
+            theme: 'erlang-dark',
         }
         return (
-            <CodeMirror value = {this.props.value} options = {options} height = "100%"/>
+            <CodeMirror value = {this.props.value}  onChange = {this.updateCode} options = {options} height = "100%"/>
         );
     }
     
