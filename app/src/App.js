832349f48ce5f2974bfb5319fb020cc24f6f45c6
nysteo <teonys@nyu.edu>
2020-09-08 21:16:47 -0400

    editor working with markdown

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SplitPane from 'react-split-pane';
-
-function App() {
+import Editor from './editor.js';
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = { markdownSrc: "# Hello World" , }
+  }
+  render() {
     return (
       <div className = "App">
         <SplitPane split = "vertical" defaultSize = "50%">
           <div className = "editor-panel">
+            <Editor className = "editor" value = {this.state.markdownSrc}/>
           </div>
           <div className = "view-pane">          
           </div>
         </SplitPane>
       </div>
     );
-  
+
+  }
+
 }
 
 export default App;
